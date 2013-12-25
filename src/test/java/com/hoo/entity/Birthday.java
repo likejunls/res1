e4a4d7c243dbcc7b8967d63ae9a06c817d16376b
likejun <likejunls@163.com>
2013-12-25 11:19:24 +0800

    初始化项目测试

---
+++ src/test/java/com/hoo/entity/Birthday.java
@@ -0,0 +1,26 @@
+package com.hoo.entity;
+
+public class Birthday {
+	private String birthday;
+
+	public Birthday(String birthday) {
+		super();
+		this.birthday = birthday;
+	}
+
+	public String getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+	public Birthday() {
+	}
+
+	@Override
+	public String toString() {
+		return this.birthday;
+	}
+}
